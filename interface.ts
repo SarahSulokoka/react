581cc5a70e4d10c87180ba0030633fd4ceb4c6ba
Sara <sarahsulokoka@gmail.com>
2025-09-19 19:07:35 +0300

    feat: function.js added

--- interface.ts
+++ interface.ts
@@ -4,7 +4,7 @@ interface User {
     isAdmin?: boolean;  //isAdmin can be optional , thats why we used ? 
 }
 
-type User = {
+type User1 = {
     name: string;
     age: number;
     isAdmin?: boolean;
@@ -15,6 +15,36 @@ interface User {
     lastname: string;
 }
 
+
+type ID = string | number ;
+
+let id: ID = "CF8"
+let id: ID = 25;
+
+
+//Tuple 
+
+type Point = [number, number];
+const p:  Point = [10, 20] 
+
+
+
 //const usr: User = {name: "Maria", age: 25, isAdmin: false}
+
+
+//Intersection
+
+type Person = {name: string};
+type Employee = {company: string};
+
+//type EmployeeInfo = Person & Employee;
+
+type EmployeeInfo = {
+    name: string;
+    company: string 
+}
+
+const e: EmployeeInfo = {name: "John", company: "AUEB"}
+
 
 
